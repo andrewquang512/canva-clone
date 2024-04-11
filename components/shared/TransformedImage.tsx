349ad0792061c231b88d40f8d9dcd60b05c52b5c
andrewquang512 <andrew.viquang@gmail.com>
2024-04-11 17:05:11 +0700

    feat(core): complete functionalities

--- components/shared/TransformedImage.tsx
+++ components/shared/TransformedImage.tsx
@@ -1,7 +1,9 @@
+'use client';
+
 import React from 'react';
 import Image from 'next/image';
-import { dataUrl, debounce, getImageSize } from '@/lib/utils';
-import { CldImage } from 'next-cloudinary';
+import { dataUrl, debounce, download, getImageSize } from '@/lib/utils';
+import { CldImage, getCldImageUrl } from 'next-cloudinary';
 import { PlaceholderValue } from 'next/dist/shared/lib/get-img-props';
 
 const TransformedImage = ({
@@ -13,7 +15,22 @@ const TransformedImage = ({
   setIsTransforming,
   hasDownload = false,
 }: TransformedImageProps) => {
-  const downloadHandler = () => {};
+  const downloadHandler = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    // Learn: prevent default behavior of browser ( which may do a Reload)
+    e.preventDefault();
+
+    download(
+      getCldImageUrl({
+        width: image?.width,
+        height: image?.height,
+        src: image?.publicId,
+        ...transformationConfig,
+      }),
+      title,
+    );
+  };
 
   return (
     <div className="flex flex-col gap-4">
@@ -49,19 +66,20 @@ const TransformedImage = ({
             onError={() => {
               debounce(() => {
                 setIsTransforming && setIsTransforming(false);
-              }, 8000);
+              }, 8000)();
             }}
             {...transformationConfig}
           />
 
           {isTransforming && (
-            <div>
+            <div className="transforming-loader">
               <Image
                 src="/assets/icons/spinner.svg"
                 width={50}
                 height={50}
-                alt="Transforming"
+                alt="spinner"
               />
+              <p className="text-white/80">Please wait...</p>
             </div>
           )}
         </div>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // Learn: in Image tag, When using an external URL, you must add it to remotePatterns in next.config.js. This action is Tt protect your application from malicious users, configuration is required in order to use external images
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+                port: ''
+            }
+        ]
+    }
+};
 
 export default nextConfig;
